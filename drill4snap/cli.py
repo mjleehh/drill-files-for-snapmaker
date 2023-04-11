b4c87e070abc29ebf7e087fe58d9faeef4e4cd7d
Michael Lee <michael.lee@wwe-media.eu>
2023-04-11 09:57:20 +0200

    rename package and add boundary calculations

--- drill4snap/cli.py
+++ drill4snap/cli.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
-
+from enum import Enum
 from pathlib import Path
 
 import typer
 
-from snapmaker_drill.parser import ExcellonFileParser
-from snapmaker_drill.writer import GcodeWriter
+from drill4snap.parser import ExcellonFileParser
+from drill4snap.writer import GcodeWriter
+
+
+class DeviceTypeArgs(str, Enum):
+    s1 = 's1'
+    s2s = 's2s'
+    s2m = 's2m'
+    s2l = 's2l'
+    s3 = 's3'
+
+
+DEVICE_TYPE_ARG_TO_NAME_MAPPING = {
+    's1': 'original',
+    's2s': 'a150',
+    's2m': 'a250',
+    's2l': 'a350',
+    's3': 'a400',
+}
 
 
 def main(
         safe_height: float = typer.Option(25.0, help='travel height above work origin in mm'),
         spindle_power: int = typer.Option(100, help='percentage of maximum spindle power'),
+        device: DeviceTypeArgs = typer.Option(DeviceTypeArgs.s2l, help='specify the target device'),
         drill_file: str = typer.Argument(..., help='Excellon .drl file to convert'),
 ):
     """
@@ -23,9 +41,15 @@ def main(
     Licenced under LGPL
     """
 
+    print(device)
+
     result = ExcellonFileParser(in_file_path=drill_file).parse()
     for tool_job in result.values():
-        GcodeWriter(tool_job, spindle_power=spindle_power, safe_height=safe_height).write(Path(drill_file).stem)
+        GcodeWriter(
+            tool_job, DEVICE_TYPE_ARG_TO_NAME_MAPPING[device],
+            spindle_power=spindle_power,
+            safe_height=safe_height
+        ).write(Path(drill_file).stem)
 
 
 def cli():
